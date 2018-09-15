a589ec50f8abe473f1bc045bbb5f6297e73ec9a8
Craig Treasure <craigktreasure@outlook.com>
2018-09-15 13:27:28 -0700

    Updated TsPropertyAttribute
      - I'm not convinced that it makes sense to rename a property given the goals of the project to reflect truth.

--- SymbioticTS.Abstractions/TsPropertyAttribute.cs
+++ SymbioticTS.Abstractions/TsPropertyAttribute.cs
@@ -2,38 +2,22 @@
 
 namespace SymbioticTS.Abstractions
 {
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
     public class TsPropertyAttribute : Attribute
     {
         /// <summary>
-        /// Gets a value indicating whether the property should be optional.
+        /// Gets the property options.
         /// </summary>
-        /// <value><c>true</c> if optional; otherwise, <c>false</c>.</value>
-        public bool AsOptional { get; }
+        /// <value>The options.</value>
+        public PropertyOptions Options { get; }
 
         /// <summary>
-        /// Gets a value indicating whether the property should be read only.
+        /// Initializes a new instance of the <see cref="TsPropertyAttribute" /> class.
         /// </summary>
-        /// <value><c>true</c> if read only; otherwise, <c>false</c>.</value>
-        public bool AsReadOnly { get; }
-
-        /// <summary>
-        /// Gets the name of the TypeScript object generated.
-        /// </summary>
-        /// <value>The name.</value>
-        public string Name { get; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TsPropertyAttribute"/> class.
-        /// </summary>
-        /// <param name="name">The property name. Auto generated if null.</param>
-        /// <param name="asReadOnly">Generate the property as 'readonly' if set to <c>true</c>.</param>
-        /// <param name="asOptional">Generate the property as optional if set to <c>true</c>.</param>
-        public TsPropertyAttribute(string name = null, bool asReadOnly = false, bool asOptional = false)
+        /// <param name="options">The property options.</param>
+        public TsPropertyAttribute(PropertyOptions options = PropertyOptions.Auto)
         {
-            this.Name = name;
-            this.AsReadOnly = asReadOnly;
-            this.AsOptional = asOptional;
+            this.Options = options;
         }
     }
 }
