c2e70174db0b7f6070ae01a2f96320c0f96ed858
Craig Treasure <craigktreasure@outlook.com>
2018-10-28 07:35:55 -0700

    Updated the CLI to support static assembly file resolution

--- SymbioticTS.Cli/ProgramOptions.cs
+++ SymbioticTS.Cli/ProgramOptions.cs
@@ -5,6 +5,12 @@ namespace SymbioticTS.Cli
     internal class ProgramOptions
     {
         /// <summary>
+        /// Gets or sets the path to an assembly references file.
+        /// </summary>
+        [Option("assembly-references-file", HelpText = "The path to an assembly references file used for assembly resolution.")]
+        public string AssemblyReferencesFilePath { get; set; }
+
+        /// <summary>
         /// Gets or sets the input assembly path.
         /// </summary>
         [Option('i', "input", Required = true, HelpText = "The path to the assembly to be transformed.")]
